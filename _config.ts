40f4d6e30662958940a6763b297a10b6d46b2a2f
Jeremy Karlsson <jeremy@barlingshult.se>
2025-08-14 00:00:37 +0200

    Update lume

--- _config.ts
+++ _config.ts
@@ -10,7 +10,6 @@ import og_images from "lume/plugins/og_images.ts";
 import pagefind from "lume/plugins/pagefind.ts";
 import picture from "lume/plugins/picture.ts";
 import reading_info from "lume/plugins/reading_info.ts";
-import relations from "lume/plugins/relations.ts";
 import relativeUrls from "lume/plugins/relative_urls.ts";
 import resolve_urls from "lume/plugins/resolve_urls.ts";
 import robots from "lume/plugins/robots.ts";
@@ -20,29 +19,27 @@ import transform_images from "lume/plugins/transform_images.ts";
 
 const site = lume();
 
-site.loadAssets([".css"]);
+site.add([".css", ".woff2"]);
+site.add("/img");
 
 site.use(esbuild());
-site.use(inline());
 site.use(lightningcss());
+site.use(slugify_urls());
+site.use(picture());
+site.use(transform_images());
+site.use(inline());
 site.use(metas());
 site.use(minify_html());
-site.use(multilanguage());
+site.use(multilanguage({
+  languages: ["sv", "en"],
+}));
 site.use(nav());
 site.use(og_images());
 site.use(pagefind());
-site.use(picture());
 site.use(reading_info());
-site.use(relations());
-site.use(relativeUrls({
-    extentions: [".html", ".css"]
-}));
+site.use(relativeUrls());
 site.use(resolve_urls());
 site.use(robots());
 site.use(sitemap());
-site.use(slugify_urls());
-site.use(transform_images({
-    extensions: [".jpg"]
-}));
 
 export default site;
